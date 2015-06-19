49b81465dee3ae341c647d90d681c6be49dc0a6e
Brian Johnson <brian@Brians-MacBook-Pro.local>
2015-06-18 21:10:37 -0400

    added function playToFive

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,16 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    var move = getInput();
-    return move;
+    return (move) ? move : getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    var move = randomPlay();
-    return move;
+    return (move) ? move : randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -74,8 +72,23 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        if (winner = "player") {
+            console.log('Player wins!');
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+            playerWins += 1;
+        }   else if (winner = "computer") {
+            console.log('Computer wins!');
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+            computerWins += 1;
+        }   else {
+            console.log('It was a tie!');
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        }
     return [playerWins, computerWins];
 }
 
